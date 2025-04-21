f33c1bb1f3c91a115ca82995715728baeb58ef60
ruma847 <rumaambarish@gmail.com>
2025-04-21 21:48:19 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,11 @@
+Age = int(input("enter the age "))
+
+if Age >= 18: 
+     print ("you are allowed to vote")
+else:
+     print ("you are not allowed to vote")
+
+if Age > 60:
+     print ("you are retired")
+else:
+     print ("you are not retired")
